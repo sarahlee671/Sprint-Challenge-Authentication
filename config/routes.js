2e3a0d61ebe42bcee0d6ecd5fe4aac9b25d0e9d4
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-06-14 09:45:26 -0700

    added register function

--- config/routes.js
+++ config/routes.js
@@ -16,20 +16,34 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+  db('users')
+    .insert(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(err => {
+      res.status(500).json(error);
+    })
 
   
 }
 
 function login(req, res) {
   // implement user login
-  let creds = req.body;
+  let {username, password} = req.body;
   db('users')
-    .where({username: creds.username})
+    .where({username})
     .first()
     .then(user => {
-      if (user && bcrypt.compareSync(creds.password, user.password)) {
+      if (user && bcrypt.compareSync(password, user.password)) {
         const token = generateToken(user)
-        res.status(200).json(token)
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token
+        })
       } else {
         res.status(401).json({message: 'Invalid Credentials'})
       }
